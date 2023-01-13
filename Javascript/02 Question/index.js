c8a9b251db836a6705584d13c194099a8d723407
Shivansh dwivedi <shivansh18181818@gmail.com>
2023-01-13 22:50:15 +0530

    navbar and hero section

--- Javascript/02 Question/index.js
+++ Javascript/02 Question/index.js
@@ -5,3 +5,5 @@ let country = "India"
 let age = 22
 
 console.log(`My name is ${firstName} ${lastName}, I am ${age} years young , I live in ${country} and i am ${maritalStatus?"married":"unmarried"}`)
+
+console.log(typeof(age));
