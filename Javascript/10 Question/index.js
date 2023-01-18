3ad4fb348b82cef1e8918ac7b6b1d46c0fbcc84f
Shivansh dwivedi <shivansh18181818@gmail.com>
2023-01-18 22:06:08 +0530

    new assignments

---
+++ Javascript/10 Question/index.js
@@ -0,0 +1,33 @@
+// 10. Figure out the result of the following comparison expression first without using console.log(). After you decide the result confirm it using console.log()
+//     - 4 > 3 = true
+//     - 4 >= 3 = true
+//     - 4 < 3 = false
+//     - 4 <= 3 = false
+//     - 4 == 4 = true
+//     - 4 === 4 = true
+//     - 4 != 4 = false
+//     - 4 !== 4 = true
+//     - 4 != '4' = false
+//     - 4 == '4' = true
+//     - 4 === '4' = false
+//     - Find the length of python and jargon and make a falsy comparison statement.
+
+
+// Checking output
+console.log(`4>3 is ${4>3}`);
+console.log(`4>=3 is ${4>=3}`);
+console.log(`4<3 is ${4<3}`);
+console.log(`4<=3 is ${4<=3}`);
+console.log(`4==4 is ${4==4}`);
+console.log(`4===4 is ${4===4}`);
+console.log(`4!=4 is ${4!=4}`);
+console.log(`4!=='4' is ${4!=='4'}`);
+console.log(`4!='4' is ${4!='4'}`);
+console.log(`4=='4' is ${4=='4'}`);
+console.log(`4==='4' is ${4==='4'}`);
+
+// Length of python and jargon
+console.log("python".length);
+console.log("jargon".length);
+// Falsy comparison
+console.log("python".length != "jargon".length);
